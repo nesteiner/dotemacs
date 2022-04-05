3628358008f39d1047a3900804dc1cb44a08b919
nesteiner <46296608+nesteiner@users.noreply.github.com>
2022-04-05 16:37:41 +0800

    add config

---
+++ config/config-gc.el
@@ -0,0 +1,7 @@
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+(provide 'config-gc)
